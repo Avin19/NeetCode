65feebe4aa19c9b2ed185e940e4d7556781f6cf4
Avin19 <avinashy1993@gmail.com>
2024-09-06 20:26:58 +0530

    TwoIntegerSum

--- TwoIntegerSum/Program.cs
+++ TwoIntegerSum/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+int[] nums = { 4, 5, 6 };
+int target = 10;
+
+Solution solution = new Solution();
+int[] output = solution.TwoSum(nums, target);
+foreach (int i in output)
+{
+    Console.WriteLine(i);
+}
+
--- TwoIntegerSum/Solution.cs
+++ TwoIntegerSum/Solution.cs
@@ -3,6 +3,18 @@ public class Solution
 {
     public int[] TwoSum(int[] nums, int target)
     {
+        for (int i = 0; i <= nums.Length - 1; i++)
+        {
+            int diff = target - nums[i];
+            foreach (int j in nums)
+            {
+                if (diff == j)
+                {
+                    return [i, j];
+                }
+            }
 
+        }
+        return null;
     }
 }
