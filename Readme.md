024fb60f23549ca922e39cc062f8d04ff48dfce6
Avinash Yadav <avinashy1993@gmail.com>
2026-06-27 18:17:52 +0530

    Tempature coversion

--- Readme.md
+++ Readme.md
@@ -9,3 +9,5 @@ dotnet run
 2. IsAnagram - 6 sept
 3. Two Integer Sum - 6 sept
 4. Read Radius of a circle - 27 June
+5. Calculate the Distance between two point of the coordinate - 27 June
+6. Tempature Conversion
---
+++ TempatureConversion/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine(" Tempature Conversion Menu");
+Console.WriteLine("1. Fahrenheit to Celsius");
+Console.WriteLine("2. Celsius to Fahrenheit");
+int choice = int.Parse(Console.ReadLine());
+if (choice == 1)
+{
+    System.Console.WriteLine("Now converting F to C ");
+    System.Console.WriteLine("Enter tempature in Fahrenheit");
+    float temp = float.Parse(Console.ReadLine());
+    float reu = (float)(temp - 32f) / 1.8f;
+    Console.WriteLine("The tempature is Celsius is : " + reu);
+}
+else
+{
+    Console.WriteLine(" Celsius to Fahrenheit");
+    Console.WriteLine(" Enter tempature in Celsius");
+    float temp = float.Parse(Console.ReadLine());
+    float rev = 1.8f * (temp + 32);
+    Console.WriteLine(" The Tempature is Fahrenheit" + rev);
+
+
+}
